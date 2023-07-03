075cd598497099bd389016401b43ee986e735ffc
senakorkmaz <senakorkmaz06@hotmail.com>
2023-07-03 23:56:49 +0300

    Controllers added

---
+++ ChatApp/Controllers/PhotoViwerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViwerViewController.swift
+//  ChatApp
+//
+//  Created by Senanur Korkmaz on 3.07.2023.
+//
+
+import UIKit
+
+class PhotoViwerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

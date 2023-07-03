3c417540f3d946ee87f2c49ac2456027ab469a4d
senakorkmaz <senakorkmaz06@hotmail.com>
2023-07-03 23:26:34 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Senanur Korkmaz on 3.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
